0ad86594619afea9a336d6f50ae76cb61d847c4a
JulianH2 <hernandez.hjose083.2023@gmail.com>
2025-04-20 05:49:25 -0600

    Final

--- app.py
+++ app.py
@@ -918,8 +918,8 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8000,
+        port=int(os.getenv("PORT", 8000)),  # usa el puerto asignado por Railway
         reload=True,
         log_level="info",
-        workers=1  
+        workers=1
     )
